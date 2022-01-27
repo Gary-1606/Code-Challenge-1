777dd7dfef407ae57de5f0d888768b277aae9730
gary-akqa <gary.kumar@akqa.com>
2022-01-27 23:01:08 +1300

    Home page added

---
+++ packman-simulation/src/packman-app/pages/home-board.page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { CenteredContainer } from "shared/components/centered-container.component";
+
+export const HomeBoardPage: React.FC = () => {
+    return (
+        <>
+            <h2>Welcome to the PACKMAN GAME !!!</h2>
+            <CenteredContainer>
+                Trial
+            </CenteredContainer>
+        </>
+    )
+}
