3d91b4e8a0fda6ba0d0a18815ac678b848d38508
gary-akqa <gary.kumar@akqa.com>
2022-01-27 22:53:02 +1300

    Created react app

---
+++ packman-simulation/src/App.tsx
@@ -0,0 +1,11 @@
+import { HomeBoardPage } from "packman-app/pages/home-board.page";
+import React from "react";
+import "./App.css";
+
+function App() {
+  return <div className="App">
+    <HomeBoardPage />
+  </div>;
+}
+
+export default App;
